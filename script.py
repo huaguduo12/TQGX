a5f51bad9554205eb53c339b256f07a70d9d4d48
hgd <huaguduo34@gmail.com>
2025-01-17 21:48:09 +0800

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ REPO_NAME = "huaguduo12/ceshi"  # 替换为你的存储库名称
 FILE_PATH = "ADD.txt"  # 替换为文件路径（如 data.txt）
 
 # 要读取的网页
-WEBPAGE_URL = "https://example.com"  # 替换为目标网页 URL
+WEBPAGE_URL = "https://tqyb.jzhou9096.workers.dev"  # 替换为目标网页 URL
 
 def fetch_webpage_content(url):
     try:
